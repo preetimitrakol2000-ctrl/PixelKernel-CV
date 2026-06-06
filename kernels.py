9cc29fed82704fbb30774f3627661e24f81e5b55
Preeti Mitra <preeti.mitra.kol2000@gmail.com>
2026-06-06 23:15:14 +0530

    Add standard CV convolution kernel arrays

---
+++ kernels.py
@@ -0,0 +1,12 @@
+# Standard CV Convolution Kernel Arrays
+SOBEL_HORIZONTAL = [
+    [-1, -2, -1],
+    [ 0,  0,  0],
+    [ 1,  2,  1]
+]
+
+SHARPEN = [
+    [ 0, -1,  0],
+    [-1,  5, -1],
+    [ 0, -1,  0]
+]
